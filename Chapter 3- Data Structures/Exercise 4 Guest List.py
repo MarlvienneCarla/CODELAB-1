0955c8b131c841ac9a581b7dd871fc6b56e80a1e
Malrvienne Carla Ramos <113139263+MarlvienneCarla@users.noreply.github.com>
2022-11-05 04:08:35 +0400

    Delete Chapter 3- Data Structures directory

--- Chapter 3- Data Structures/Exercise 4 Guest List.py
+++
@@ -1,4 +0,0 @@
-name = ['Claire', 'Eurille', 'Louise']
-print("Are you available for dinner, " + name[0].title() + "?")
-print("Do you want some dinner, " + name[1].title() + "?")
-print("Are you free for later's dinner, " + name[2].title() + "?")
